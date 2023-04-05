ce1a6d677837a304063cbd90535d9cf9d0de15a8
Devaansh-Kumar <devaanshk840@gmail.com>
2023-04-05 13:02:24 +0530

    added env variables to mysqlBackup file

--- src/mysqlBackup.sh
+++ src/mysqlBackup.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
-mkdir mysql_backups
-docker exec mysql mysqldump -uroot -ppassword src_development > "$HOME"/mysql_backups/mysql_backup.sql
+
+source .env
+docker exec mysql mysqldump -u$DB_USER -p$MYSQL_ROOT_PASSWORD src_development > "$HOME"/mysql_backups/mysql_backup.sql
