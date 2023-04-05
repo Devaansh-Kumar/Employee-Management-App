243402adf088233b08ee50a7b7e6423428d771ca
Devaansh-Kumar <devaanshk840@gmail.com>
2023-04-05 12:53:46 +0530

    corrected the mysqlBackup file

--- src/mysqlBackup.sh
+++ src/mysqlBackup.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 mkdir mysql_backups
-docker exec mysql mysqldump -u root -p password src_development > "$HOME"/mysql_backups/mysql_backup.sql
+docker exec mysql mysqldump -uroot -ppassword src_development > "$HOME"/mysql_backups/mysql_backup.sql
